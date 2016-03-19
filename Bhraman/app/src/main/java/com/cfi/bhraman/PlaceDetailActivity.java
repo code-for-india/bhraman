c1bdd96b929573d828c7b86aae775b8fabda36e2
chhavip <coolaqua12@gmail.com>
2016-03-19 15:41:35 +0530

    Added QR

--- Bhraman/app/src/main/java/com/cfi/bhraman/PlaceDetailActivity.java
+++ Bhraman/app/src/main/java/com/cfi/bhraman/PlaceDetailActivity.java
@@ -1,17 +1,31 @@
 package com.cfi.bhraman;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.encoder.ByteMatrix;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 import com.wdullaer.materialdatetimepicker.time.TimePickerController;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
+import net.glxn.qrgen.android.QRCode;
+import net.glxn.qrgen.core.scheme.VCard;
+
 import java.util.Calendar;
+import java.util.Hashtable;
 
 /**
  * Created by chhavi on 19/3/16.
@@ -19,6 +33,7 @@ import java.util.Calendar;
 public class PlaceDetailActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     EditText dateChoose;
     EditText time_choose;
+    LinearLayout chooseOthers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +73,43 @@ public class PlaceDetailActivity extends AppCompatActivity implements DatePicker
                 dpd.show(getFragmentManager(), "Datepickerdialog");
             }
         });
+        chooseOthers = (LinearLayout)findViewById(R.id.choose_others);
+
+        final String[] others = {"Abhishek", "Yogesh", "Aanchal"};
+
+      /*  chooseOthers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(PlaceDetailActivity.this)
+                        .title("Your Members")
+                        .items(others)
+                        .itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+                                *//**
+                                 * If you use alwaysCallMultiChoiceCallback(), which is discussed below,
+                                 * returning false here won't allow the newly selected check box to actually be selected.
+                                 * See the limited multi choice dialog example in the sample project for details.
+                                 **//*
+                                return true;
+                            }
+                        })
+                        .positiveText("Ok")
+                        .show();
+            }
+        });*/
+
+        VCard johnDoe = new VCard("John Doe")
+                .setEmail("john.doe@example.org")
+                .setAddress("John Doe Street 1, 5678 Doestown")
+                .setTitle("Mister")
+                .setCompany("John Doe Inc.")
+                .setPhoneNumber("1234")
+                .setWebsite("www.example.org");
+        Bitmap myBitmap =  QRCode.from(johnDoe).bitmap();
+       // Bitmap myBitmap = QRCode.from("www.example.org").bitmap();
+        ImageView myImage = (ImageView) findViewById(R.id.imageview);
+        myImage.setImageBitmap(myBitmap);
     }
 
     @Override
@@ -70,4 +122,23 @@ public class PlaceDetailActivity extends AppCompatActivity implements DatePicker
 
         time_choose.setText(hourOfDay+" : " + minute);
     }
+
+   /* public static Bitmap generateQrCode(String myCodeText) throws WriterException {
+        Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H); // H = 30% damage
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        int size = 256;
+
+        ByteMatrix bitMatrix = qrCodeWriter.encode(myCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
+        int width = bitMatrix.width();
+        Bitmap bmp = Bitmap.createBitmap(width, width, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < width; y++) {
+                bmp.setPixel(y, x, bitMatrix.get(x, y)==0 ? Color.BLACK : Color.WHITE);
+            }
+        }
+        return bmp;
+    }*/
 }
---
+++ Bhraman/app/src/main/java/com/cfi/bhraman/SummaryActivity.java
@@ -0,0 +1,25 @@
+package com.cfi.bhraman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+    /*    VCard johnDoe = new VCard("John Doe")
+                .setEmail("john.doe@example.org")
+                .setAddress("John Doe Street 1, 5678 Doestown")
+                .setTitle("Mister")
+                .setCompany("John Doe Inc.")
+                .setPhoneNumber("1234")
+                .setWebsite("www.example.org");
+        QRCode.from(johnDoe).file();*/
+
+    }
+}
