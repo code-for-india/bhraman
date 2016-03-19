c8d42570653c525b38f1066e4360405f0575feef
chhavip <coolaqua12@gmail.com>
2016-03-19 10:08:07 +0530

    Added cards and something

--- Bhraman/app/src/main/java/com/cfi/bhraman/GroupsMainActivity.java
+++ Bhraman/app/src/main/java/com/cfi/bhraman/GroupsMainActivity.java
@@ -1,9 +1,51 @@
 package com.cfi.bhraman;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chhavi on 19/3/16.
  */
 public class GroupsMainActivity extends AppCompatActivity {
+
+
+    ListView groupsList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.groups_main);
+        groupsList = (ListView)findViewById(R.id.groups_list);
+
+        ArrayList<String> groups = new ArrayList<>();
+        groups.add("Family");
+        groups.add("Friends");
+     //   ArrayAdapter<String> adapter = new ArrayAdapter<String>(R.layout.groups_item_layout, R.id.group_name, groups);
+
+
+    }
+
+    public class GroupsListAdapter extends ArrayAdapter<Person>{
+
+        List<Person> persons;
+        Context context;
+        public GroupsListAdapter(Context context, List objects) {
+            super(context, 0, objects);
+            this.context = context;
+            this.persons = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return super.getView(position, convertView, parent);
+        }
+    }
 }
---
+++ Bhraman/app/src/main/java/com/cfi/bhraman/Person.java
@@ -0,0 +1,11 @@
+package com.cfi.bhraman;
+
+/**
+ * Created by chhavi on 19/3/16.
+ */
+public class Person {
+
+    String name;
+    String id;
+    int imageResouce;
+}
